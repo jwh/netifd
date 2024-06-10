956368de9de0e0e81f443d5be3a96baa769db978
Joe Holden <jwh@zorins.us>
2024-06-10 00:20:06 +0000

    xdp: add skel xdp.c

---
+++ xdp.c
@@ -0,0 +1,14 @@
+/* xdp stuff */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <bpf/bpf.h>
+#include <bpf/libbpf.h>
+#include <xdp/libxdp.h>
+#include <linux/err.h>
+#include <linux/netdev.h>
